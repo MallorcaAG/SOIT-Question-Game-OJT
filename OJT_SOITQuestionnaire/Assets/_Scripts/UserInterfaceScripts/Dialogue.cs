469f75553956f08818305943a6fd264b8727abad
MallorcaAG <129373604+MallorcaAG@users.noreply.github.com>
2024-07-17 11:36:54 +0800

    Adjusted Dialogue Timing

--- OJT_SOITQuestionnaire/Assets/_Scripts/UserInterfaceScripts/Dialogue.cs
+++ OJT_SOITQuestionnaire/Assets/_Scripts/UserInterfaceScripts/Dialogue.cs
@@ -3,25 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
-enum DialogueType
-{
-    Static, DynamicResults
-}
 
 public class Dialogue : MonoBehaviour
 {
+    [SerializeField] private GameEvent onDialogueDone;
     [SerializeField] private TextMeshProUGUI ui;
     [SerializeField] private string[] lines;
     [SerializeField] private float textSpeed;
     [SerializeField] private float textDelayBeforeNextLine = 3f;
-    [SerializeField] private DialogueType dialogueType;
-    [SerializeField] private GameEvent onDescribeCourse;
+
 
 
     private int index;
-    private Vector2[] scores;
+    private int num = 0;
     private bool isTyping;
-
+    private bool endReached = false;
     private float remainingTime;
 
     private void Start()
@@ -29,17 +25,17 @@ public class Dialogue : MonoBehaviour
         remainingTime = textDelayBeforeNextLine;
     }
 
-    public void Initialize(Component sender, object data)
+    public void Initialize()
     {
-        scores = (Vector2[])data;
-
-        if(dialogueType == DialogueType.DynamicResults)
-        {
-            DialogueBuilder();
-        }
-
+        gameObject.SetActive(true);
         ui.text = string.Empty;
         StartDialogue();
+    }
+
+    public void setDialogue(Component sender, object data)
+    {
+        lines = (string[])data;
+        Initialize();
     }
 
     private void Update()
@@ -55,24 +51,37 @@ public class Dialogue : MonoBehaviour
             {
                 NextLine();
                 remainingTime = textDelayBeforeNextLine;
+                //Debug.Log("next line");
             }
             else
             {
                 StopAllCoroutines();
                 ui.text = lines[index];
+
             }
+
+            if(index >= lines.Length - 1 && endReached == false)
+            {
+                //Debug.Log("I ran");
+                num++;
+                onDialogueDone.Raise(this, num);
+                endReached = true;
+            }
+            
         }
     }
 
     void StartDialogue()
     {
         index = 0;
+
         StartCoroutine(TypeLine());
 
     }
 
     IEnumerator TypeLine()
     {
+        endReached = false;
         if (lines[index] == null) yield break;
         foreach (char c in lines[index].ToCharArray())
         {
@@ -97,103 +106,5 @@ public class Dialogue : MonoBehaviour
         }
     }
     
-    void DialogueBuilder()
-    {
-        int linesScriptLength = 20;
-        lines = new string[linesScriptLength];
-
-        string course1 = GameResultsDisplay.getCategory((int)scores[0].x);
-        int course1Score = (int)scores[0].y;
-        string course2 = GameResultsDisplay.getCategory((int)scores[1].x);
-        int course2Score = (int)scores[1].y;
-        string lastCourse = GameResultsDisplay.getCategory((int)scores[4].x);
-        int lastCourseScore = (int)scores[4].y;
-
-
-        float mean = (scores[0].y + scores[1].y + scores[2].y + scores[3].y + scores[4].y)/5;
-        float course1ToMeanDiff = course1Score - mean;
-        float course1ToCourse2Diff = course1Score - course2Score;
-
-        lines[0] = "The results are in!";
-        lines[1] = " ";
-        lines[2] = "You’re a "+course1+" major!";
-        lines[3] = "Scoring a "+course1Score.ToString()+", "+course1ToMeanDiff.ToString()+" points off the average.";
-        lines[4] = "Your second highest course is "+course2+ " with a score of "+course2Score.ToString();
-        lines[5] = "That's a " + course1ToCourse2Diff.ToString() + " point difference from your top scoring course.";
-        lines[6] = "Your lowest was "+lastCourse+ " with a score of "+lastCourseScore.ToString();
-        switch((Category)scores[0].x)
-        {
-            case Category.CS:
-                onDescribeCourse.Raise(this, Category.CS);
-
-                lines[7] = " ";
-
-                lines[8] = "Computer Science is . . .";
-                lines[9] = "among us";
-                lines[10] = "sus";
-                lines[11] = "ding ding ding ding";
-                lines[12] = "test text";
-                break;
-
-            case Category.IT:
-                onDescribeCourse.Raise(this, Category.IT);
-
-                lines[7] = " ";
-
-                lines[8] = "Information Technology is , no stranger to love";
-                lines[9] = "IT knows the rules, and so do I";
-                lines[10] = "A full commitments what IT's looking for";
-                lines[11] = "Never gunna give you up";
-                lines[12] = "test text";
-                break;
-
-            case Category.IS:
-                onDescribeCourse.Raise(this, Category.IS);
-
-                lines[7] = " ";
-
-                lines[8] = "Information System is . . .";
-                lines[9] = "Skibidi dop dop dop";
-                lines[10] = "yes yes";
-                lines[11] = "Skibidi";
-                lines[12] = "test text";
-                break;
-
-            case Category.EMC:
-                onDescribeCourse.Raise(this, Category.EMC);
-
-                lines[7] = " ";
-
-                lines[8] = "Entertainment and Multimedia Computing is. . .";
-                lines[9] = "shikanokonokokoshitantan";
-                lines[10] = "shikanokonokokoshitantan";
-                lines[11] = "shikanokonokokoshitantan";
-                lines[12] = "test text";
-                break;
-
-            case Category.DS:
-                onDescribeCourse.Raise(this, Category.DS);
-
-                lines[7] = " ";
-
-                lines[8] = "Data Science is . . .";
-                lines[9] = "text";
-                lines[10] = "test";
-                lines[11] = "E";
-                lines[12] = "test text";
-                break;
-
-            default:
-
-                break;
-        }
-
-        lines[13] = "Feel free to download the data as reference if you so desire.";
-    }
-
-
-
-
-
 
 }
---
+++ OJT_SOITQuestionnaire/Assets/_Scripts/UserInterfaceScripts/DialogueBuilder.cs
@@ -0,0 +1,159 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueBuilder : MonoBehaviour
+{
+    [SerializeField] private GameEvent sendDialogue;
+    [SerializeField] private string[] lines;
+    private Vector2[] scores;
+    private int num = 0;
+
+    // Start is called before the first frame update
+    public void Initialize(Component sender, object data)
+    {
+        if (data != null)
+        {
+            scores = (Vector2[])data;
+        }
+
+        switch(num)
+        {
+            case 0:
+                TestResultsDialogue();
+                break;
+
+            case 1:
+                CourseDialogue();
+                break;
+
+            case 2:
+                ContactAndInfoDialogue();
+                break;
+        }
+        
+    }
+
+    public void getNum(Component sender, object data)
+    {
+        num = (int)data;
+
+        Initialize(sender, null);
+    }
+
+    void TestResultsDialogue()
+    {
+        int linesScriptLength = 8;
+        lines = new string[linesScriptLength];
+
+        string course1 = GameResultsDisplay.getCategory((int)scores[0].x);
+        int course1Score = (int)scores[0].y;
+        string course2 = GameResultsDisplay.getCategory((int)scores[1].x);
+        int course2Score = (int)scores[1].y;
+        string lastCourse = GameResultsDisplay.getCategory((int)scores[4].x);
+        int lastCourseScore = (int)scores[4].y;
+
+
+        float mean = (scores[0].y + scores[1].y + scores[2].y + scores[3].y + scores[4].y) / 5;
+        float course1ToMeanDiff = course1Score - mean;
+        float course1ToCourse2Diff = course1Score - course2Score;
+
+        lines[0] = "The results are in!";
+        lines[1] = " ";
+        lines[2] = "You’re a " + course1 + " major!";
+        lines[3] = "Scoring a " + course1Score.ToString() + ", " + course1ToMeanDiff.ToString("#.##") + " points off the average.";
+        lines[4] = "Your second highest course is " + course2 + " with a score of " + course2Score.ToString();
+        lines[5] = "That's a " + course1ToCourse2Diff.ToString("#.##") + " point difference from your top scoring course.";
+        lines[6] = "Your lowest was " + lastCourse + " with a score of " + lastCourseScore.ToString();
+        lines[7] = "    ";
+
+
+        sendDialogue.Raise(this, lines);
+    }
+
+    void CourseDialogue()
+    {
+        int linesScriptLength = 6;
+        lines = new string[linesScriptLength];
+
+        switch ((Category)scores[0].x)
+        {
+            case Category.CS:
+
+                lines[0] = " ";
+
+                lines[1] = "Computer Science delves into the complex behaviors of computers, understanding the theory behind software to innovative ways of utilizing them in the real world.";
+                lines[2] = "It challenges its students to design complex algorithms and solve computing problems through complex hands-on lab activities.";
+                lines[3] = "In CS, you will learn data structures and algorithms, artificial intelligence, machine learning, web development, and much more.";
+                lines[4] = "Our CS program offers specializations in Intelligent Systems, Software Engineering, and Application Development.";
+
+                break;
+
+            case Category.IT:
+
+                lines[0] = " ";
+
+                lines[1] = "Information Technology explores the information age of today’s world, managing the vast and versatile technological field with efficient hardware and software solutions.";
+                lines[2] = "It empowers its students with skills necessary to manage, process, and exchange information securely over the network through practical hands-on training.";
+                lines[3] = "In IT, you will learn networking and cloud computing, IT support and troubleshooting, database management, and much more.";
+                lines[4] = "Our IT program offers specializations in Computer Network and Security, Cybersecurity, Application Development, and Enterprise Data Management.";
+
+                break;
+
+            case Category.IS:
+
+                lines[0] = " ";
+
+                lines[1] = "Information System utilizes technology to drive business success, creating value through an effective and efficient streamlining of business processes.";
+                lines[2] = "It molds its students to become business technology experts, developing strong technical skills to implement computing-based solutions in the management of information systems.";
+                lines[3] = "In IS, you will learn to leverage technology for business process analysis, business intelligence, planning, data management, and much more.";
+                lines[4] = "Our IS program offers specializations in Business Analytics, Enterprise Resource Planning, Enterprise Data Management, IT Audit, and IT Service Management.";
+
+                break;
+
+            case Category.EMC:
+
+                lines[0] = " ";
+
+                lines[1] = "Entertainment and Multimedia Computing examines the captivating capabilities of human creativity combined with cutting-edge technology, creating impactful and engaging experiences that entertain, educate, and inspire.";
+                lines[2] = "It inspires its students to unleash their creativity, designing and developing various entertainment media, and build video game project prototypes to solve technical gaming challenges.";
+                lines[3] = "In EMC, you will learn digital art and animation, game design, user experience design, game physics programming, and much more.";
+                lines[4] = "Our EMC program mainly specializes in Game Design and Development";
+
+                break;
+
+            case Category.DS:
+
+                lines[0] = " ";
+
+                lines[1] = "Data Science investigates the trends and patterns hidden within large sets of data, extracting insights to make data-driven decisions.";
+                lines[2] = "It equips its students with in-demand skills to leverage opportunities in data and strategically use math and statistics to fully analyze all available information and come to a well-informed conclusion.";
+                lines[3] = "In DS, you will learn data analysis and visualization, big data, machine learning, and much more.";
+                lines[4] = "Our DS program is highly specialized resulting in a more focused approach.";
+
+                break;
+
+            default:
+
+                break;
+
+        }
+
+        lines[5] = " For more information, please feel free to visit the official Mapua website, or contact our university’s admission office";
+
+        sendDialogue.Raise(this, lines);
+    }
+
+    void ContactAndInfoDialogue()
+    {
+        int linesScriptLength = 2;
+        lines = new string[linesScriptLength];
+
+        lines[0] = "";
+
+        lines[1] = "Feel free to download the data as reference if you so desire.";
+
+        sendDialogue.Raise(this, lines);
+    }
+
+}
